224ab04664b95ba800be97154e39deb75125afe1
carloguenzl <carlo@guenzl.eu>
2025-10-25 19:25:47 +0200

    start choice

--- generator.h
+++ generator.h
@@ -5,13 +5,30 @@
 #include <vector>
 #include <numeric>
 #include "k_gram_map.h"
+#include <random>
 using namespace std;
 
 class Generator {
 public:
     int output_length;
     KGramMap MP;
-    Generator(string input, int k, int out_length): MP(input,k),output_length(out_length) {
+    mt19937 rng;
+    string output;
 
+    Generator(string input, int k, int out_length): MP(input,k),output_length(out_length) {
+        random_device rd;
+        rng.seed(rd());
+        output = choose_start();
     }
+
+    string choose_start(){
+        vector<float> weights = MP.vfrequencies();
+        discrete_distribution<int> dist(weights.begin(),weights.end());
+        int index = dist(rng);
+        return MP.outer_keys[index];
+    }
+    
+
+
+
 };

096246bd49d9da5c26bb5ff9f8b222ecfc89fce6
carloguenzl <carlo@guenzl.eu>
2025-10-25 17:45:36 +0200

    up2date with k_gram_map.h and some more test lines

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main(int argc, char* argv[]) {
     /* if(argc!=4){
         cerr << "Usage: <size of chunks> <input.txt> <size of output>"<< endl;
         exit(1);
-    } */
+    } 
+    */
     int k = atoi(argv[1]);
     string input_file = argv[2];
     int output_n = atoi(argv[3]);
@@ -29,12 +30,15 @@ int main(int argc, char* argv[]) {
 
     vector<string> outer_keys = MP.outer_keys;
     vector<char> inner_keys = MP.get_inner_keys(outer_keys[n]);
-    int value = MP.mp [outer_keys[n]] [inner_keys[m]];
+    vector<int> values = MP.get_values(outer_keys[n]);
+    
+    
     cout << n<< "th outer Key: " << outer_keys[n] << endl;
     cout << m<< "th inner Key: " << inner_keys[m] << endl;
-    cout << "Value: " << value << endl;
-    cout << outer_keys.size() << " " << inner_keys.size() << endl;
-
-
+    cout << "Value: " << values[m] << endl;
+    cout << "key vector sizes: "<<outer_keys.size() << " " << inner_keys.size() << " " << values.size()<< endl;
+    cout << "probabilitiy: " << MP.probability(outer_keys[n],inner_keys[m]) << endl;
+    float freq =MP.frequency(outer_keys[n]);
+    cout << "frequency: " << freq << endl;
     return 0;
 }

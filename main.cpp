26b1598844020970c61cb054a7a3530d3268bd15
carloguenzl <carlo@guenzl.eu>
2025-10-25 15:13:49 +0200

    fixed k_gram_map again, introduced vectors containing the keys, some test output logic in main.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,33 @@
 #include <string>
 #include <fstream>
 #include <map>
+#include <vector>
 using namespace std;
 
 map<string,map<char,int>> k_gram_map(int k, string &input) {
     map<string, map<char,int>> mp;
-    for(int i; i<input.size();i++) { // check k_gram and next char for each i
+    for(int i=0; i<input.size();i++) { // check k_gram and next char for each i
         string current_kgram = input.substr(i,k); // slice from start i to len k
         char next_char = input[i+k];
         mp [current_kgram] [next_char]++;
-
     }
     return mp;
+}
+
+vector<string> get_outer_keys(map<string,map<char,int>> const &input_map) {
+    vector<string> keys;
+    for(auto const &element:input_map){
+        keys.push_back(element.first);
+    }
+    return keys;
+}
+
+vector<char> get_inner_keys(map<char,int> const &input_map) {
+    vector<char> keys;
+    for(auto const &element:input_map){
+        keys.push_back(element.first);
+    }
+    return keys;
 }
 
 
@@ -32,7 +48,18 @@ int main(int argc, char* argv[]) {
     while(getline(f,line)){
         input+=line;
     }
-    cout << input[15] << endl;
+
+    map<string, map<char,int>> mp = k_gram_map(3,input);
+
+
+    int n = 10;
+    int m = 0;
+    vector<string> outer_keys = get_outer_keys(mp);
+    vector<char> inner_keys = get_inner_keys(mp[outer_keys[n]]);
+    int value = mp [outer_keys[n]] [inner_keys[m]];
+    cout << n<< "th outer Key: " << outer_keys[n] << endl;
+    cout << m<< "th inner Key: " << inner_keys[m] << endl;
+    cout << "Value: " << value << endl;
 
     return 0;
 }

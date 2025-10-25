ea202bde1470fee8fef83894ac1905d735c009bf
carloguenzl <carlo@guenzl.eu>
2025-10-25 20:40:05 +0200

    next character choice algo and generating for-loop

--- generator.h
+++ generator.h
@@ -6,6 +6,7 @@
 #include <numeric>
 #include "k_gram_map.h"
 #include <random>
+#include <algorithm>
 using namespace std;
 
 class Generator {
@@ -13,12 +14,15 @@ public:
     int output_length;
     KGramMap MP;
     mt19937 rng;
+    string current_gram;
     string output;
+
 
     Generator(string input, int k, int out_length): MP(input,k),output_length(out_length) {
         random_device rd;
         rng.seed(rd());
-        output = choose_start();
+        current_gram = choose_start();
+        output = "";
     }
 
     string choose_start(){
@@ -27,8 +31,25 @@ public:
         int index = dist(rng);
         return MP.outer_keys[index];
     }
-    
 
+    char choose_next_character(){
+        auto it = find(MP.outer_keys.begin(), MP.outer_keys.end(), current_gram); //check if current_gram is in the map 
+        if(it!=MP.outer_keys.end()){
+            vector<char> chars = MP.get_inner_keys(current_gram);
+            vector<float> weights = MP.vprobabilities(current_gram);
+            discrete_distribution<int> dist(weights.begin(),weights.end());
+            int index = dist(rng);
+            cout << chars[index] << endl;
+            return chars[index]
+        }
+        else{
+            current_kgram = choose_start();
+            choose_next_character();
+        }
+    }
 
-
-};
+    void generate(){
+        for(int i=0,i<=output_length,i++){
+            output+=choose_next_character();
+        }
+    }

877bed9a961d8d3f9b832bfc7186c7c4ce97918e
carloguenzl <carlo@guenzl.eu>
2025-10-25 16:49:24 +0200

    added get_values and probability calculator

--- k_gram_map.h
+++ k_gram_map.h
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <map>
 #include <vector>
+#include <numeric>
 using namespace std;
 
 class KGramMap {
@@ -30,5 +31,20 @@ public:
             keys.push_back(element.first);
         }
         return keys;
+    }
+
+    vector<int> get_values(string const outer_key) {
+        vector<int> values;
+        for(auto const &element:mp[outer_key]){
+            values.push_back(element.second);
+        }
+        return values;
+    }
+
+    float probability(string const outer_key, char const inner_key) {
+        vector<int> values = get_values(outer_key);
+        int character_count = values[inner_key];
+        int total_count = accumulate(values.begin(),values.end(),0);
+        return character_count/total_count
     }
 };

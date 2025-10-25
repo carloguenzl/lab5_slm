be8817b09c02dd07b3789c90b74176b14d7c42c9
carloguenzl <carlo@guenzl.eu>
2025-10-25 15:51:45 +0200

    summarized the map stuff in the class KGramMap

---
+++ k_gram_map.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class KGramMap {
+public:
+    string input;
+    int k;
+    map<string,map<char,int>> mp;
+    vector<string> outer_keys;
+
+    k_gram_map(const string inp,int n): input(inp),k(n) {
+        for(int i=0; i+k<input.size();i++) {
+            string current_kgram = input.substr(i,k);
+            char next_char = input[i+k];
+            mp [current_kgram] [next_char]++;
+        }
+
+        for(auto const &element:mp){
+            outer_keys.push_back(element.first);
+        }
+    }
+
+    vector<char> get_inner_keys(string const outer_key) {
+        vector<char> keys;
+        for(auto const &element:mp[outer_key]){
+            keys.push_back(element.first);
+        }
+        return keys;
+    }
+};
